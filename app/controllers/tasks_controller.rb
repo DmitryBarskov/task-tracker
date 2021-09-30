e6e63c73074b770645a1f942c4fb997d904491cb
Amir-Gaifullin <amir.gaifullin@yandex.ru>
2021-09-30 12:49:16 +0300

    new_ver1

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+
+	before_action :set_task, only: [:show, :destroy]
+	def index
+		@tasks = Task.all
+	end
+
+	def show
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		Task.new(task_params)
+
+		if task.save
+			redirect_to task_path(task), notice: 'Task has ben created'
+		else
+			render :new
+		end
+	end
+
+	def destroy
+		@task.destroy!
+
+		redirect_to task_path
+	end
+
+	private
+
+	def set_task
+		@task = Task.find_by!(id: params[:id])
+	end
+
+	def task_params
+		params.permit(:project_id, :title, :description, :deadline_at)
+	end	
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   resources :projects
+  # in projects have:
+  # get '/projects', controller: :projects,action: :index
+  # get '/projects/:id', controller: :projects,action: :show
+  # post '/projects', controller: :projects, action: :create
+  resources :tasks
+
 end
