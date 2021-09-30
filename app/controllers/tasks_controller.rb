23207f8ab2bba8f25c87fe1a9e577557672bb820
Amir-Gaifullin <amir.gaifullin@yandex.ru>
2021-09-30 14:32:49 +0300

    final_app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
 	end
 
 	def create
-		Task.new(task_params)
+		task = Task.new(task_params)
 
 		if task.save
 			redirect_to task_path(task), notice: 'Task has ben created'
