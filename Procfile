000810a40939bb7a2cc2b1445ad03afbe1a24d1f
RamilGatin <nais.gatin@yandex.ru>
2021-10-07 15:49:30 +0300

    simple_form,slim,boost,heroku

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: bundle exec Puma
+release: bundle exec rake db:migrate
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  validates :titile, presence: true
+  #validates :title, lenght: { minimum:20 }
+  #validates :deadline_at, presence: true
 end
