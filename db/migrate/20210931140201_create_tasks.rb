f3a5506bd9a4cd5f1eebc31e9b65ad1a40011f5e
Amir-Gaifullin <amir.gaifullin@yandex.ru>
2021-10-15 14:35:24 +0300

    fix_2

--- db/migrate/20210931140201_create_tasks.rb
+++ db/migrate/20210931140201_create_tasks.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string :title, null: false
+      t.string :title
       t.text :description
       t.datetime :deadline_at
       t.references :project, null: false, foreign_key: true
