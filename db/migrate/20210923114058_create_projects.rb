fe77024dd0b1361842269838b4de76d5f23a0c93
rav <nurgalievrav@gmail.com>
2021-10-28 17:13:41 +0300

    small upd

--- db/migrate/20210923114058_create_projects.rb
+++ db/migrate/20210923114058_create_projects.rb
@@ -1,4 +1,4 @@
-class CreateTasks < ActiveRecord::Migration[6.1]
+class CreateProjects < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.string :title, null: false
