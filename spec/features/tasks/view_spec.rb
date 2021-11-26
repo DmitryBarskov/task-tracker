bfb28beea41a6b88c5884b5f36200f094a5759cc
AlexanderTheThird <khusaenov02@gmail.com>
2021-11-26 17:09:29 +0300

    Add description and deadline checking

--- spec/features/tasks/view_spec.rb
+++ spec/features/tasks/view_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.describe 'View a task', type: :feature do
   include_context 'when user signed in'
 
-  let(:task) { create(:task, title: 'A test task') }
+  let(:task) { create(:task, title: 'A test task', description: 'A test description', deadline_at: 'November 27, 2021 23:59') }
 
   scenario 'User views a task' do
     visit task_url(task)
 
     expect(page).to have_content('A test task')
+    expect(page).to have_content('A test description')
+    expect(page).to have_content('November 27, 2021 23:59')
   end
 end
 
