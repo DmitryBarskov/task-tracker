2f72fc039c6cc6e65bcf87962cca6ddfbcd38c23
AlexanderTheThird <khusaenov02@gmail.com>
2021-11-27 21:36:20 +0300

    Add sending notification to update project

---
+++ app/interactors/update_project/send_notifications.rb
@@ -0,0 +1,29 @@
+class UpdateProject
+  class SendNotifications
+    include Interactor
+
+    delegate :project, :current_user, to: :context
+    delegate :users, to: :project, prefix: true
+
+    def call
+      send_email_notifications
+      create_activity
+    end
+
+    private
+
+    def create_activity
+      RegisterActivityJob.perform_later(current_user.id, "project_updated", project.id, "Project")
+    end
+
+    def send_email_notifications
+      project_users.find_each do |user|
+        send_email_notification(project, user)
+      end
+    end
+
+    def send_email_notification(project, user)
+      ProjectMailer.project_updated(project, user).deliver_later
+    end
+  end
+end
