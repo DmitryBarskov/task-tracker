72e4f8e650d1ca41994a464e0d06c94f32ec75a1
AlexanderTheThird <khusaenov02@gmail.com>
2021-11-27 21:17:37 +0300

    Add params for FilteredTasksQuery

--- app/query_objects/filtered_tasks_query.rb
+++ app/query_objects/filtered_tasks_query.rb
@@ -1,5 +1,5 @@
 class FilteredTasksQuery < BaseFilteredQuery
-  ALLOWED_PARAMS = [:status, :title]
+  ALLOWED_PARAMS = [:status, :title, :description, :deadline_at]
 
   def by_status(relation, status)
     relation.where(status: status)
@@ -7,5 +7,13 @@ class FilteredTasksQuery < BaseFilteredQuery
 
   def by_title(relation, title)
     relation.where("title LIKE ?", title)
+  end
+
+  def by_description(relation, description)
+    relation.where("description LIKE ?", description)
+  end
+
+  def by_deadline_at(relation, deadline)
+    relation.where(deadline_at: deadline)
   end
 end
