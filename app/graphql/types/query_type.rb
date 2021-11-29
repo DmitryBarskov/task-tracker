d46cf4d59e4638ba544c08d23ca47b312c1efc82
Ramzes2002 <ramaznui2002@gmail.com>
2021-11-29 23:11:02 +0300

    Types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,8 +3,9 @@ module Types
     # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
     include GraphQL::Types::Relay::HasNodeField
     include GraphQL::Types::Relay::HasNodesField
-
     field :projects, resolver: Resolvers::Projects
     field :project, resolver: Resolvers::Project
+    field :users, resolver: Resolvers::Users
+    field :user, resolver: Resolvers::User
   end
 end
