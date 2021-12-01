d505c8bcc9e6bd58edb50aff81b471aebb2481c7
unknown <siva4iov1705@gmail.com>
2021-12-02 01:58:00 +0300

    altered schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_185354) do
+ActiveRecord::Schema.define(version: 2021_11_11_132317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_185354) do
   create_table "projects_users", force: :cascade do |t|
     t.integer "user_id"
     t.integer "project_id"
+    t.string "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -57,9 +58,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_185354) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status", default: "not_started", null: false
-    t.bigint "user_id", null: false
     t.index ["project_id"], name: "index_tasks_on_project_id"
-    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -72,5 +71,4 @@ ActiveRecord::Schema.define(version: 2021_11_15_185354) do
   end
 
   add_foreign_key "tasks", "projects"
-  add_foreign_key "tasks", "users"
 end
