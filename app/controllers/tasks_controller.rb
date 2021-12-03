f1db35d68079e6de384e4620b413f15907fef9fd
AlexanderTheThird <khusaenov02@gmail.com>
2021-12-03 14:28:23 +0300

    Remove duplicate line

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
-  before_action :set_task, only: %i[show edit update destroy]
   before_action -> { authorize @task }, only: %i[show edit update destroy]
   before_action -> { authorize Task }, only: %i[index new create]
 
